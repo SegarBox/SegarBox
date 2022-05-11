afa2504682eab5aa14295c8f7f38aad3a5e285c6
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-05-11 11:43:45 +0700

    temp toolbar

---
+++ app/src/main/java/com/example/bungkusyuk/ui/activity/DetailActivity.kt
@@ -0,0 +1,76 @@
+package com.example.bungkusyuk.ui.activity
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.widget.NestedScrollView
+import com.example.bungkusyuk.R
+import kotlin.math.max
+import kotlin.math.min
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var scrollView: NestedScrollView
+    private var mInitialStatusBarColor = 0
+    private var mFinalStatusBarColor = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        mToolbar = findViewById(R.id.toolbar)
+        scrollView = findViewById(R.id.scrollView)
+
+        mToolbar.background.alpha = 0
+        setSupportActionBar(mToolbar)
+
+        mInitialStatusBarColor = Color.TRANSPARENT
+        mFinalStatusBarColor = ContextCompat.getColor(this, R.color.white)
+
+        scrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+            val headerHeight: Int = 100
+            var ratio = 0f
+            if (scrollY > 0 && headerHeight > 0) ratio =
+                min(max(scrollY, 0), headerHeight).toFloat() / headerHeight
+            val newAlpha = (ratio * 255).toInt()
+
+            // Set Toolbar
+            mToolbar.background.alpha = newAlpha
+
+            // Set Toolbar Title
+            if (ratio >= 0.65F) {
+                mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.purple_700))
+            } else {
+                mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+            }
+
+            // Set Status Bar
+//            updateStatusBarColor(ratio)
+        })
+    }
+
+    private fun updateStatusBarColor(scrollRatio: Float) {
+        val r = interpolate(Color.red(mInitialStatusBarColor),
+            Color.red(mFinalStatusBarColor),
+            1 - scrollRatio)
+        val g = interpolate(Color.green(mInitialStatusBarColor),
+            Color.green(mFinalStatusBarColor),
+            1 - scrollRatio)
+        val b = interpolate(Color.blue(mInitialStatusBarColor),
+            Color.blue(mFinalStatusBarColor),
+            1 - scrollRatio)
+
+        val window: Window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.statusBarColor = Color.rgb(r, g, b)
+    }
+
+    private fun interpolate(from: Int, to: Int, param: Float): Int {
+        return (from * param + to * (1 - param)).toInt()
+    }
+}
--- app/src/main/java/com/example/bungkusyuk/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/bungkusyuk/ui/activity/MainActivity.kt
@@ -1,12 +1,127 @@
 package com.example.bungkusyuk.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.NestedScrollView
 import com.example.bungkusyuk.R
+import com.example.bungkusyuk.databinding.ActivityMainBinding
+import kotlin.math.max
+import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private var ratio = 0F
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
     }
+
+    private fun init() {
+        setToolbar()
+        binding.btnDetail.setOnClickListener(this)
+    }
+
+    private fun setToolbar() {
+        // Hooks
+        val colorStrokeBelowRatio = ColorStateList(
+            arrayOf(
+                intArrayOf(-android.R.attr.state_focused),
+                intArrayOf(android.R.attr.state_focused),
+            ),
+
+            intArrayOf(
+                ContextCompat.getColor(this, R.color.white),
+                ContextCompat.getColor(this, R.color.white)
+            )
+        )
+
+        val colorStrokeAboveRatio = ColorStateList(
+            arrayOf(
+                intArrayOf(-android.R.attr.state_focused),
+                intArrayOf(android.R.attr.state_focused),
+            ),
+
+            intArrayOf(
+                ContextCompat.getColor(this, R.color.purple_700),
+                ContextCompat.getColor(this, R.color.purple_700)
+            )
+        )
+
+        // Initial Set Toolbar
+        binding.toolbar.background.alpha = 0
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.tiSearch.apply {
+            setBoxStrokeColorStateList(colorStrokeBelowRatio)
+            defaultHintTextColor = colorStrokeBelowRatio
+            hintTextColor = colorStrokeBelowRatio
+        }
+
+
+        // On Scrolled
+        binding.scrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+            val headerHeight: Int = binding.header.height - binding.toolbar.height
+            ratio = min(max(scrollY, 0), headerHeight).toFloat() / headerHeight
+            val newAlpha = (ratio * 255).toInt()
+
+            // Set Toolbar
+            binding.tvRatio.text = newAlpha.toString()
+            binding.toolbar.background.alpha = newAlpha
+
+            // Set Toolbar Items
+            if (ratio >= 0.65F) {
+                binding.tiSearch.apply {
+                    setBoxStrokeColorStateList(colorStrokeAboveRatio)
+                    defaultHintTextColor = colorStrokeAboveRatio
+                    hintTextColor = colorStrokeAboveRatio
+                }
+            } else {
+                binding.tiSearch.apply {
+                    setBoxStrokeColorStateList(colorStrokeBelowRatio)
+                    defaultHintTextColor = colorStrokeBelowRatio
+                    hintTextColor = colorStrokeBelowRatio
+                }
+            }
+
+        })
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_detail -> {
+                startActivity(Intent(this, DetailActivity::class.java))
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val newAlpha = (ratio * 255).toInt()
+        binding.toolbar.background.alpha = newAlpha
+
+        if (ratio >= 0.65F) {
+            binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.purple_700))
+        } else {
+            binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
