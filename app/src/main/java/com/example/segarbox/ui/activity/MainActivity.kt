d922f9d24f392c607df9fa5454f88d2ddd1b682e
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-05-16 03:15:27 +0700

    fixed poppins bold, build temp main activity

--- app/src/main/java/com/example/segarbox/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/segarbox/ui/activity/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.widget.NestedScrollView
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.segarbox.R
 import com.example.segarbox.data.local.datastore.SettingPreferences
 import com.example.segarbox.databinding.ActivityMainBinding
