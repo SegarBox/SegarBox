bb9ff44fbee9bf0490e798856a8aa8fae7f632c3
Gabriella Teshalonika Gondokusumo <gtg.gabriella@gmail.com>
2022-05-19 22:40:17 +0700

    Check1

--- app/src/main/java/com/example/segarbox/data/local/model/DummyModel.kt
+++ app/src/main/java/com/example/segarbox/data/local/model/DummyModel.kt
@@ -7,3 +7,11 @@ data class DummyModel(
     val qty: String = "100g / pack",
     val price: String = "Rp 12000"
 )
+
+data class DummyModelInProgress(
+    val firstItemImage: Int = R.drawable.cauliflowers,
+    val firstItemName: String = "Bayam",
+    val firstItemPrice: Int = 4000,
+    val firstItemCount: Int = 1,
+
+)
